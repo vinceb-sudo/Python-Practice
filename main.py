724d6cfdb4d5cfc578a81f7e53ed11ed888f4bbf
vinceb-sudo <vinceb@wsc.school.nz>
2026-07-01 14:57:25 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ start_code = input("Press enter or return to continue.")
 print ("")
 user_answer = input("Question 1: what color is a black hole?: ")
 if user_answer == "black" or user_answer == "Black":
-    print (user_answer)
+    print ("")
     print ("Correct! Black holes are black.")
 else:
+    print ("")
     print ("Incorrect. The correct answer is black.")
-
+print ("")
+print ("")
 user_answer = input("Question 2: what is the food related to black holes?: ")
 if user_answer == "spaghetti" or user_answer == "Spaghetti":
-    print (user_answer)
+    print ("")
     print ("Correct! Spaghetti is related to black holes because of the term 'spaghettification'. ")
 else:
+    print ("")
     print ("Incorrect. The correct answer is spaghetti.")
-    
+print ("")
+print ("")
 user_answer = input("Question 3: are black holes real?: ")
-if user_answer == "yes" or user_answer == "Yes":
-    print (user_answer)
+if user_answer == "yes" or user_answer == "Yes" or user_answer == "y" or user_answer == "Y" or user_answer == "yeah" or user_answer == "Yeah" or user_answer == "yep" or user_answer == "Yep" or user_answer == "yup" or user_answer == "Yup" or user_answer == "true" or user_answer == "True" or user_answer == "t" or user_answer == "T" or user_answer == "correct" or user_answer == "Correct":
+    print ("")
     print ("Correct! Black holes are real.")
 else: 
+    print ("")
     print ("Incorrect. The correct answer is yes.")
